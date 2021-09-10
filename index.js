37b57fc9f321ab321225cd0b727b89d1800a262b
Gautam Vaja <83920497+gautamVaja@users.noreply.github.com>
2021-09-10 22:28:07 +0530

    Passport for user signin

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
-const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
+//session cookie
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local");
 
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
-
-
-// use express router
-app.use('/', require('./routes'));
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
+app.use(
+  session({
+    name: "codeial",
+    secret: "something", //change secrete for production
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: `${1000 * 60 * 5}`,
+    },
+  })
+);
 
-app.listen(port, function(err){
-    if (err){
-        console.log(`Error in running the server: ${err}`);
-    }
+app.use(passport.initialize());
+app.use(passport.session());
 
-    console.log(`Server is running on port: ${port}`);
+// use express router
+app.use("/", require("./routes"));
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
+
+  console.log(`Server is running on port: ${port}`);
 });
