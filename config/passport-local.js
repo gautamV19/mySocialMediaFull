a0b6d15fa29a8ba7a528c2061fbaee27158e678a
Gautam Vaja <83920497+gautamVaja@users.noreply.github.com>
2021-09-10 21:39:00 +0530

    Passport set up

---
+++ config/passport-local.js
@@ -0,0 +1,48 @@
+const passport = require("passport");
+
+const LocalStratergy = require("passport-local").Strategy;
+
+const User = require("../models/user");
+
+// authentication using passport
+passport.use(
+  new LocalStratergy(
+    {
+      usernameField: "email",
+    },
+    function (email, password, done) {
+      //find a user and establish the identity
+      User.findOne({ email: email }, function (err, user) {
+        if (err) {
+          console.log("Error in finding user -- inside passport");
+          return done(err);
+        }
+
+        if (!user || user.password != password) {
+          console.log("Invalide username/password");
+          return done(null, false);
+        }
+
+        return done(null, user);
+      });
+    }
+  )
+);
+
+//serializing the user to decide which is to be kept in the cookies
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+//deserializing the user from the key in the cookies
+passport.deserializeUser(function (id, done) {
+  User.findOne(id, function (err, user) {
+    if (err) {
+      console.log("Error in finding user -- inside passport");
+      return done(err);
+    }
+    return done(null, user);
+  });
+});
+
+module.exports = passport;
