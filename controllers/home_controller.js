cb3b10d611b32edf29be7c959ca181f2750f43eb
Gautam Vaja <gatuamiitmee@gmail.com>
2021-09-13 21:19:39 +0530

    post with author name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,17 +3,31 @@ const Post = require("../models/post");
 module.exports.home = function (req, res) {
   //   console.log(req.cookies);
   //   res.cookie("user_id", 25);
-  Post.find({}, function (err, posts) {
-    if (err) {
-      console.log("Error in showing post");
-      return;
-    }
-    console.log("your posts for home page", posts);
-    return res.render("home", {
-      title: "Home",
-      posts: posts,
+  // Post.find({}, function (err, posts) {
+  //   if (err) {
+  //     console.log("Error in showing post");
+  //     return;
+  //   }
+  //   console.log("your posts for home page", posts);
+  //   return res.render("home", {
+  //     title: "Home",
+  //     posts: posts,
+  //   });
+  // });
+
+  Post.find({})
+    .populate("user")
+    .exec(function (err, posts) {
+      if (err) {
+        console.log("Error in showing post");
+        return;
+      }
+      console.log("your posts for home page", posts);
+      return res.render("home", {
+        title: "Home",
+        posts: posts,
+      });
     });
-  });
 };
 
 // module.exports.actionName = function(req, res){}
