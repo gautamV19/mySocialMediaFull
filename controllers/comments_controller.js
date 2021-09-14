6c543df643072cfe2c58d51d0b78166ed3e8fc43
Gautam Vaja <gatuamiitmee@gmail.com>
2021-09-14 18:39:04 +0530

    Delete comment done

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -43,3 +43,36 @@ module.exports.create = function (req, res) {
 
   return res.redirect("back");
 };
+
+module.exports.destroy = function (req, res) {
+  Comment.findById(req.params.id, function (err, comment) {
+    if (err) {
+      console.log("Error in finding comment for deleting");
+      return;
+    }
+    // .id is to converting into string ideally it should be ._id
+    console.log("******** Comment to be deleted", comment.content);
+    if (comment.user == req.user.id) {
+      let postId = comment.post;
+
+      comment.remove();
+
+      Post.findByIdAndUpdate(postId, {
+        $pull: {
+          comments: req.params.id,
+          function(err) {
+            if (err) {
+              console.log("Error in removing comment from post", err);
+              return;
+            }
+            return res.redirect("/");
+          },
+        },
+      });
+      return res.redirect("/");
+    } else {
+      console.log("Unable to delete");
+      return res.redirect("back");
+    }
+  });
+};
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -24,7 +24,7 @@ module.exports.destroy = function (req, res) {
       console.log("Error in finding post for deleting");
     }
     // .id is to converting into string ideally it should be ._id
-    if (post.user == req.user.id) {
+    if (post && post.user == req.user.id) {
       post.remove();
       Comment.deleteMany({ post: req.params.id }, function (err) {
         if (err) console.log("Error in deleting the post", err);
