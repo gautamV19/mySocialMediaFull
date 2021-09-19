59f6136a5c612f92a9115ef0830683b199516295
Gautam Vaja <gatuamiitmee@gmail.com>
2021-09-19 17:08:19 +0530

    show and delete post

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,40 @@
+const Post = require("../../../models/post");
+const Comment = require("../../../models/comment");
+
+module.exports.index = async function (req, res) {
+  let posts = await Post.find({})
+    .sort("-createdAt")
+    .populate("user", "name id email")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+        select: "name id email",
+      },
+    });
+  return res.json(200, {
+    message: "List of posts",
+    posts: posts,
+  });
+};
+
+module.exports.destroy = async function (req, res) {
+  try {
+    let post = await Post.findById(req.params.id);
+    // if (post && post.user == req.user.id) {
+    post.remove();
+    await Comment.deleteMany({ post: req.params.id });
+    return res.status(200).json({
+      message: "Post deleted successfully!",
+    });
+    // } else {
+    // req.flash("error", "Unauthorize: You can't delete this post");
+    // return res.status(401).send("Unauthorize");
+    // }
+  } catch (err) {
+    console.log("******Error", err);
+    return res.json(500, {
+      message: "Internal server error",
+    });
+  }
+};
