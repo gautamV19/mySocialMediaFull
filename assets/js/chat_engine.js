a66a99e29d8a12a419e65272852a7517cfeb3b1f
Gautam Vaja <gatuamiitmee@gmail.com>
2021-09-26 20:40:50 +0530

    chat engine connected

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,19 +1,20 @@
-class chatEngine {
-  constructor(chatBoxoId, userEmail) {
-    this.chatBoxoId = ${`#${chatBoxoId}`};
+class ChatEngine {
+  constructor(chatBoxId, userEmail) {
+    this.chatBox = $(`#${chatBoxId}`);
     this.userEmail = userEmail;
 
-    this.socket = io.connect("http://localhost:5000");
+    this.socket = io.connect("http://localhost:5000", {
+      transports: ["websocket"],
+    });
 
-    if(this.userEmail){
-        this.connectionHandler();
+    if (this.userEmail) {
+      this.connectionHandler();
     }
-
   }
 
-  connectionHandler(){
-      this.socket.io("connect", function () { 
-          console.log("Connection established");
-       } )
+  connectionHandler() {
+    this.socket.on("connect", function () {
+      console.log("connection established using sockets...!");
+    });
   }
 }
